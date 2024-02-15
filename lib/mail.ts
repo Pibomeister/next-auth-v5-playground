892da25dc568b7aa09bf4dec802eb500b5438461
Eduardo Picazo <edy7picazo@gmail.com>
2024-02-15 08:50:25 -0600

    feat: update mail domains

--- lib/mail.ts
+++ lib/mail.ts
@@ -8,7 +8,7 @@ export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmlink = `${domain}/auth/new-verification?token=${token}`;
 
   await resend.emails.send({
-    from: 'onboarding@resend.dev',
+    from: 'noreply@auth-tut.online',
     to: email,
     subject: 'Please verify your email address',
     html: `<p>Click <a href="${confirmlink}">here</a> to verify your email address.</p>`,
@@ -19,7 +19,7 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
   const resetLink = `${domain}/auth/new-password?token=${token}`;
 
   await resend.emails.send({
-    from: 'onboarding@resend.dev',
+    from: 'noreply@auth-tut.online',
     to: email,
     subject: 'Please reset your password',
     html: `<p>Click <a href="${resetLink}">here</a> to reset your password.</p>`,
@@ -28,7 +28,7 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
 
 export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
   await resend.emails.send({
-    from: 'onboarding@resend.dev',
+    from: 'noreply@auth-tut.online',
     to: email,
     subject: 'Your 2FA code',
     html: `<p>Your 2FA is: <strong>${token}</strong></p>`,
